55bf53059a6deccf2ff89a3de1daef36f0a94318
Taekwondoug <145704798+Taekwondoug@users.noreply.github.com>
2023-09-29 08:40:40 -0300

    última atualização

---
+++ cabelo/cadastrar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="shortcut icon" href="./imagens/icons8-gato-16" type="image/x-icon">
+    <title> Your Hair </title>
+</head>
+<style>
+    body{
+        background-color: #ede7f6;
+    }
+</style>
+<body>
+<main>
+        <div class="container">
+            <h1 class="center purple-text"> Produtos p/ Seu Cabelo </h1>
+            <hr>
+            <div class="row reduc">
+
+                <form action="processaC.php" method="POST">
+
+                    <div class="input-field col s12">
+                        <label> Shampoo </label><br>
+                        <input type="text" name="shampoo" required="required" />
+                    </div>
+
+                    <div class="input-field col s12">
+                        <label> Condicionador </label><br>
+                        <input type="text" name="condicionador" required="required" />
+                    </div>
+
+                    <div class="input-field col s12">
+                        <label> Máscara de cabelo </label><br>
+                        <input type="text" name="mascara" required="required" />
+                    </div>
+
+                    <div class="input-field col s12">
+                        <label> Creme </label><br>
+                        <input type="text" name="creme" required="required" />
+                    </div>
+
+                    <div class="input-field col s12">
+                        <label> Reparador de pontas </label><br>
+                        <input type="text" name="reparador" required="required" />
+                    </div>
+
+                    <div class="input-field col s12">
+                        <button class="waves-effect waves-light btn purple" type="submit" name="enviar">Enviar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ cabelo/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="shortcut icon" href="./imagens/icons8-gato-16" type="image/x-icon">
+    <title> Your Hair </title>
+</head>
+<style>
+    body{
+        background-color: #ede7f6;
+    }
+</style>
+<body>
+    <?php
+    include_once('conecta.php');
+    $conexao = conectar();
+
+    $sql = "SELECT * FROM cabelo";
+    $resultado = mysqli_query($conexao, $sql);
+
+    ?>
+    <div class="container">
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Shampoo</th>
+                    <th>Condicionador</th>
+                    <th>Máscara de cabelo</th>
+                    <th>Creme</th>
+                    <th>Reparador de pontas</th>
+                </tr>
+            </thead>
+
+            <tbody>
+            <?php while ($dados = mysqli_fetch_assoc($resultado)) : ?>
+                <tr>
+                    <td> <?php echo $dados['shampoo']; ?> </td>
+                    <td> <?php echo $dados['condicionador']; ?> </td>
+                    <td> <?php echo $dados['mascara']; ?> </td>
+                    <td> <?php echo $dados['creme']; ?> </td>
+                    <td> <?php echo $dados['reparador']; ?> </td>
+                    <td>
+                        <a href="formEdit.php?editar=<?php echo $dados['id']; ?>">
+                            <img height='24' width='24' src='img/edit.png'> </a>
+
+                        <a href="processa.php?deletar=<?php echo $dados['id']; ?>">
+                            <img height='24' width='24' src='img/del.png' /> </a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
